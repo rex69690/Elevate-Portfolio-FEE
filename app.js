1fb1c75b813486196538cf633a4a80411e6409ba
Tushar Koundal <tusharkoundal913@gmail.com>
2024-10-15 10:53:29 +0530

    new changes  for local storage implementation

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ function toggleMobileMenu() {
         menu.classList.toggle('open');
     }
 }
+
+
+const storedChanges = localStorage.getItem('userDetails');
+
+if (storedChanges) {
+    const changes = JSON.parse(storedChanges);
+
+    
+    document.getElementById('name-user').textContent = changes.username;
+    document.getElementById('bio-user').textContent = changes.username;
+    document.getElementById('card-email').textContent = `Email: ${changes.email}`
+    document.getElementById('bio-text').textContent  = `so ... ${ changes.bio_u}`;
+    
+}
